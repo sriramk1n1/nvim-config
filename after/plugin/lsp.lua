a913bc741776ce2a3786ddda7bbef9a9dfefb6aa
sriramk1n1 <sriramjkini@gmail.com>
2023-12-13 12:59:13 +0530

    initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,18 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({
+    buffer = bufnr,
+    preserve_mappings = false
+  })
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+
---
+++ lua/sk/init.lua
@@ -0,0 +1,3 @@
+require("sk.remap")
+require("sk.set")
+	
